6ad0c95bee8acfeb76acd31d4655da9a843d8211
Hasan <hasanaydins@gmail.com>
2020-03-03 21:58:04 +0300

    api server   baglandi

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -4,11 +4,20 @@ import { ListGroup, ListGroupItem } from "reactstrap";
 export  default  class CategoryList extends Component {
   state = {
     categories: [
-      { categoryId: 1, categoryName: "Beverages" },
-      { categoryId: 2, categoryName: "Condiments" },
-      { categoryId: 3, categoryName: "Beverages" }
+
     ]
   };
+
+  componentDidMount() { // componentler yerleesti simdi categorileri yerlestir demek
+      this.getCategories()
+  }
+
+    getCategories = () => {
+
+      fetch("http://localhost:3000/categories")
+          .then(response => response.json())
+          .then(data => this.setState({categories: data}))
+  }
 
   render() {
     return (
@@ -19,7 +28,7 @@ export  default  class CategoryList extends Component {
           {this.state.categories.map(category => (
             <ListGroupItem
               onClick={() => this.props.changeCategory(category)}
-              key={category.categoryId}
+              key={category.id}
             >
               {category.categoryName}
             </ListGroupItem>
