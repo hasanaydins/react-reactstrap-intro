48340881b04ac33389de850e4cf9cf53f2c1af01
Hasan <hasanaydins@gmail.com>
2020-03-03 18:54:38 +0300

    first comm react-reactstrap-intro

---
+++ src/CategoryList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CategoryList extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Category List</h3>
+            </div>
+        );
+    }
+}
+
+export default CategoryList;
---
+++ src/Navi.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Navi extends Component {
+    render() {
+        return (
+            <div>
+                <p>Navigation</p>
+            </div>
+        );
+    }
+}
+
+export default Navi;
